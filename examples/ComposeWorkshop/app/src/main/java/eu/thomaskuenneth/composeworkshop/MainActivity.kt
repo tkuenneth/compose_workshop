955f50ce7696bdf844d10a78d5af8a489502a2ae
Thomas Künneth <kuennetht@gmail.com>
2023-10-14 13:25:57 +0200

    Counter demo added

--- examples/ComposeWorkshop/app/src/main/java/eu/thomaskuenneth/composeworkshop/MainActivity.kt
+++ examples/ComposeWorkshop/app/src/main/java/eu/thomaskuenneth/composeworkshop/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
@@ -19,7 +32,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                ComposeWorkshopScreen()
+                // ComposeWorkshopScreen()
+                CounterDemo()
             }
         }
     }
@@ -38,5 +52,44 @@ fun ComposeWorkshopScreen() {
             text = "Hello Jetpack Compose",
             style = MaterialTheme.typography.headlineLarge
         )
+    }
+}
+
+@Composable
+@Preview
+fun CounterDemo() {
+    var counter by remember { mutableStateOf(0) }
+    Column(
+        horizontalAlignment = CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            contentAlignment = Center,
+            modifier = Modifier
+                .height(100.dp)
+        ) {
+            if (counter == 0) {
+                Text(
+                    text = "Noch nicht geklickt",
+                    softWrap = true,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            } else {
+                Text(
+                    text = "$counter",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+        }
+        Button(
+            onClick = { counter += 1 }
+        ) {
+            Text(text = "Klick")
+        }
     }
 }
