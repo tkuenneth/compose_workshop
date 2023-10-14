add60fe9cdf04af5279d4a0dd0c033c274b29651
Thomas Künneth <kuennetht@gmail.com>
2023-10-14 12:06:06 +0200

    Add Compose to the project

--- examples/ComposeWorkshop/app/build.gradle.kts
+++ examples/ComposeWorkshop/app/build.gradle.kts
@@ -30,6 +30,14 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -54,4 +62,28 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //////////////////
+    // Jetpack Compose
+    //////////////////
+
+    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
+    // basic APIs
+    implementation("androidx.compose.ui:ui")
+    // Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    // Material Design 3
+    implementation("androidx.compose.material3:material3")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
+    // Android Studio Preview support
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    // UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.8.0")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 }
--- examples/ComposeWorkshop/app/src/main/java/eu/thomaskuenneth/composeworkshop/MainActivity.kt
+++ examples/ComposeWorkshop/app/src/main/java/eu/thomaskuenneth/composeworkshop/MainActivity.kt
@@ -1,13 +1,42 @@
 package eu.thomaskuenneth.composeworkshop
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, getString(R.string.app_name), Toast.LENGTH_LONG).show()
+        setContent {
+            MaterialTheme {
+                ComposeWorkshopScreen()
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ComposeWorkshopScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.secondaryContainer),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello Jetpack Compose",
+            style = MaterialTheme.typography.headlineLarge
+        )
     }
 }
